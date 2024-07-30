6e046718a241f1017b0b1a725f479a51e7342a32
Aleksei Lapunov <lapunoff.alex@yandex.ru>
2024-07-31 03:10:39 +0700

    Theme 18_iterators covered

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -1,3 +1,5 @@
+//use std::collections::btree_map::Range;
+
 fn factorial(num: u64) -> u64 {
     // TODO: Complete this function to return the factorial of `num` which is
     // defined as `1 * 2 * 3 * … * num`.
@@ -10,6 +12,9 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+
+    //(2..=num).fold(1, |acc, x| acc * x)
+    (2..=num).product()
 }
 
 fn main() {
