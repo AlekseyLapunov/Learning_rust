768ad658fb1e01d3e9f9c60a9474c6dafbe6f6dc
Aleksei Lapunov <lapunoff.alex@yandex.ru>
2024-08-02 16:21:45 +0700

    Theme 22_clippy covered

--- exercises/22_clippy/clippy1.rs
+++ exercises/22_clippy/clippy1.rs
@@ -6,7 +6,8 @@
 
 fn main() {
     // TODO: Fix the Clippy lint in this line.
-    let pi = 3.14;
+    //#[allow(clippy::approx_constant)]
+    let pi = std::f32::consts::PI;
     let radius: f32 = 5.0;
 
     let area = pi * radius.powi(2);
